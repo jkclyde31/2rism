ede08490af1afcbabaea0e58901993a5a36f8ea2
jkClyde <aquin.randall@gmail.com>
2025-01-29 07:16:07 +0800

    added buttom component

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,11 +1,19 @@
+import Button from "@/components/Button";
+
 export const metadata = {
   title: "Services"
 }
 
 export default function Services() {
   return (
-    <div className="flex items-center justify-center h-[75vh]">
-      <h1 className="text-[25px]">Services Page</h1>
+    <div className="flex items-center justify-center h-[75vh] flex-col gap-5 max-w-[1200px] mx-auto">
+      <Button className="bg-black">Default Button</Button>
+      <Button variant="secondary">Secondary Button</Button>
+      <Button variant="outline" size="large">Large Outline Button</Button>
+      <Button variant="danger" isLoading>Loading Button</Button>
+      {/* <Button variant="success" icon={<Heart />}>With Icon</Button> */}
+      {/* <Button variant="ghost" icon={<ArrowRight />} iconPosition="right">Ghost Button</Button> */}
+      <Button fullWidth>Full Width Button</Button>
     </div>
   );
 }
