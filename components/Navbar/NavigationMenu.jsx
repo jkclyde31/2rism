df9f639ce096022dcaa4cebe26e459f0c375a05c
jkClyde <aquin.randall@gmail.com>
2025-02-04 07:14:21 +0800

    update navigation Menu

--- components/Navbar/NavigationMenu.jsx
+++ components/Navbar/NavigationMenu.jsx
@@ -5,21 +5,19 @@ import { usePathname } from 'next/navigation';
 const NavigationMenu = ({navLinks}) => {
     const pathname = usePathname();
   return (
-         <div className='hidden md:ml-6 md:block '>
-                  <div className='flex space-x-2'>
+                  <div className='hidden md:flex max-w-[683px] justify-between items-center gap-[10px] px-[10px] bg-[#0F0F0F] rounded-[15px]'>
                     {navLinks.map((link) => (
                       <Link
                         key={link.href}
                         href={link.href}
                         className={`${
-                          pathname === link.href ? 'bg-black' : ''
-                        } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                          pathname === link.href ? 'bg-[#1A1A1A]' : ''
+                        } text-white hover:bg-[#1A1A1A] hover:text-white rounded-md px-[14px] py-[12px]`}
                       >
                         {link.name}
                       </Link>
                     ))}
                   </div>
-                </div>
   )
 }
 
