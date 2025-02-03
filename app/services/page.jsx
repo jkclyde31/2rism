0d4d985abd1d3beb6b4ea84b5aff0868078d8a91
jkClyde <aquin.randall@gmail.com>
2025-02-04 07:40:10 +0800

    added ShadCN

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -7,7 +7,27 @@ export const metadata = {
 export default function Services() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <h1 className="text-2xl font-bold mb-4">Buttons</h1>
+        {/* Size Variations */}
+       {/* Width Variations */}
+        <h3 className="text-lg font-semibold">Width Variations</h3>
+        <div className="flex flex-col gap-4">
+        <Button className="w-full py-[19.5px] bg-black rounded-[15px] hover:bg-gray-800 hover:text-white">
+          Button Label
+        </Button>
+
+        <Button className="w-[500px] py-[15px] bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600">
+            Gradient Button
+          </Button>
+
+          <Button className="rounded-full">
+            Rounded Button
+          </Button>
+
+          <Button className="border-2 border-blue-500">
+            Custom Border
+          </Button>
+
+        </div>
  
   </div>
   );
