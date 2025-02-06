a0cc6b94eebf6daafeb7085ae98812810985a170
jkClyde <aquin.randall@gmail.com>
2025-02-07 05:34:27 +0800

    added About Us

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import PopularDestinations from "./sections/PopularDestinatins";
 import HotelsAndRestaurants from "./sections/HotelAndRestaurant";
 import TravelTipsAndAdvice from "./sections/TravelTipsAndAdvice";
 import Activities from "./sections/Activities";
+import AboutSection from "./sections/AboutSection";
 
 export const metadata = {
   title: "Homepage"
@@ -15,6 +16,9 @@ export default function Home() {
      <HotelsAndRestaurants/>
      <TravelTipsAndAdvice/>
      <Activities/>
+     <AboutSection/>
+
+
 
 
    </div>
---
+++ app/sections/AboutSection.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+
+
+const AboutSection = () => {
+  return (
+    <div className="max-w-[1128px] w-full flex justify-between gap-[15px] items-center mx-auto pt-[110px]" >
+        {/* left || Text */}
+        <div className="max-w-[548px] w-full">
+            <h2 className="text-[32px] font-semibold leading-[48px] mb-[32px]">About Us</h2>
+            <p className="text-[16px] leading-[32px] text-[#5B5B5B] mb-[32px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a sapien justo. Nulla facilisis tristique imperdiet. Nullam a placerat odio. Sed in ex augue. Aliquam porta consectetur lorem sit amet ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+            <Button className=" text-[16px] tracking-[0px] text-white px-[25px] py-[12px] bg-[#7B61FF] ">
+             Read More
+          </Button>
+        </div>
+
+        <Image
+         src={"/about.png"}
+         width={556}
+         height={488}
+        />
+
+       
+
+       
+
+    </div>
+  )
+}
+
+export default AboutSection
